77cc0bcc7d5a29b4de884f8a22043833b778e20d
vivit <kenji@vivitjp.com>
2022-02-17 19:26:12 +0900

    CSS list

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+interface IObj {
+  id: string
+  name: string
+}
+
+const sample = [
+  { "id": "234", "name": "Smith", address: "Tokyo Shibuya High Building ", zip: "012-1023" },
+  { "id": "345", "name": "Karen Abe", address: "Hokkaido Sapporo City ", zip: "070-1232" },
+]
+
+const cols = [1, 1, 3, 1];
+const colSum = cols.reduce((p, c) => p += c);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-      <h1 className="text-green-500 md:text-blue-500 lg:text-green-500">
-        Tailwind Sample
-      </h1>
-    </div>
+    <>
+      <div className="box w-full p-2 border-2 text-center text-gray-400 font-sans font-bold">
+        <div>[親] grid grid-rows-1 grid-cols-SUM grid-flow-col gap-y-1</div>
+        <div>[子] col-span-N</div>
+      </div>
+      <div className="flex flex-col p-1 bg-gray-100 ">
+        {
+          sample.map(item => {
+            const cssTR = `grid grid-rows-1 grid-cols-${colSum} grid-flow-col gap-y-1 mx-1 mb-2 shadow round`
+            return (
+              <>
+                <div className={cssTR}>
+                  {Object.entries(item).map(([key, val], idx) => {
+                    const cssTD = `col-span-${cols[idx]} text-start p-2 text-gray-500 bg-white`
+                    return <div className={cssTD}>{val}</div>
+                  })}
+                </div>
+              </>
+            )
+          })
+        }
+      </div>
+    </>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        // Simple 16 column grid
+        //'16': 'repeat(16, minmax(0, 1fr))',
+
+        // Complex site-specific column configuration
+        'mylist': '100px 200px minmax(300px, 1fr) 100px',
+      }
+    },
   },
   plugins: [],
 }
